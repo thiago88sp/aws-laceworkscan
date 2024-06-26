2d7afbc2c73a6098212287d66209bce60d526e5f
Thiago Pontes <tpontes@fortinet.com>
2024-06-26 17:37:57 -0300

    remove key pair

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,7 +61,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
-  key_name      = var.key_name
+  
   subnet_id     = aws_subnet.main.id
 
   tags = {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,9 +9,3 @@ variable "instance_type" {
   type        = string
   default     = "t2.micro"
 }
-
-variable "key_name" {
-  description = "The name of the SSH key pair to use"
-  type        = string
-  default     = "tsp.key"
-}
